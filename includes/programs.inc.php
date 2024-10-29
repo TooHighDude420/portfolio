47f505c3d90ac43c6b4a06d3701ffa259f6d6718
TooHighDude <nataro112@gmail.com>
2024-10-29 09:12:36 +0100

    29-10-2024

--- includes/programs.inc.php
+++ includes/programs.inc.php
@@ -1,1 +1,35 @@
-<h1>Programs</h1>
+<div>
+    <h1>Programs</h1>
+</div>
+
+<div class="programs-cards">
+    <div class="card-temp">
+        <div class="card-temp-top">
+            <img src="img/programs/mastermind1.png" alt="mastermind1">
+        </div>
+        <div class="card-temp-bottom">
+            <h1>Mastermind 1.0</h1>
+            <p>Het eerste project voor het vak java</p>
+        </div>
+    </div>
+
+    <div class="card-temp">
+        <div class="card-temp-top">
+
+        </div>
+        <div class="card-temp-bottom">
+            <h1>Pink goose</h1>
+            <p>Het eerste project voor het vak websites</p>
+        </div>
+    </div>
+
+    <div class="card-temp">
+        <div class="card-temp-top">
+
+        </div>
+        <div class="card-temp-bottom">
+            <h1>2048</h1>
+            <p>Mijn eerste zelf gemaakte android game</p>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <script src="javascript/matter.js"></script>
     <title>Nataro Ajaouin</title>
 </head>
 
